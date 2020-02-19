717fd199a96ebdf184573804ab796eebbbd7bd4b
marwanamr3 <marwan.abdelgawad@student.guc.edu.eg>
2020-02-19 05:35:30 +0200

    utilized bitcoin api in controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Exception\RequestException;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use GuzzleHttp\Client;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    function index(Request $request){
+        $client = new Client();
+        try {
+            $response = $client->request('GET', 'https://api.coindesk.com/v1/bpi/historical/close.json?start='.$request['from'].'&end='.$request['to']);
+            $body = $response->getBody()->getContents();
+        }catch (RequestException $e){
+            var_dump($e->getMessage());
+        }
+
+        return view('welcome', ['body' => $body]);
+    }
 }
